079ed98a45702960cfeae569d19b969f5c242e0f
wei Yuan <yuanwei12345@gmail.com>
2020-07-01 12:30:28 +0800

    pice of crap coding

---
+++ DoublePointer/wordDistance.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findClosest(self, words, word1, word2):
+        length = len(words)
+        dis = length
+        matchWord = [word1, word2]
+        i = 0
+        while i < length-1:
+            j = i + 1
+            if words[i] in matchWord:
+                while words[j] not in matchWord:
+                    if j == length-1:
+                        break
+                    j += 1
+            if words[j] not in matchWord and j == length-1:
+                return dis
+            elif words[j] in matchWord and words[i] in matchWord:
+                if words[j] == words[i]:
+                    i = j
+                else:
+                    if j - i < dis :
+                        dis = j - i
+                    i = j
+        return res
